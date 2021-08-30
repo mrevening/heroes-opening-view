5f64a9e407eeb30a96a0651329028007ee2f1309
dwieczorek <dwieczorek@kpmg.pl>
2021-08-30 22:44:06 +0200

    filter scenarios by size

--- src/ShowScenarios.js
+++ src/ShowScenarios.js
@@ -1,20 +1,27 @@
 import Button from './Button';
 import { useSelector, useDispatch } from 'react-redux';
 import { scenarioSettings } from './actions';
-import FilterScenarioSize from './FilterScenarioSize';
+// import FilterScenarioSize from './FilterScenarioSize';
 import { getScenarios } from './selectors';
+import { useState } from 'react';
 
 export default function ShowScenarios({ closeAction }){
+    const buttons = ["S", "M", "L", "X-L", ];
     const dispatch = useDispatch();
-
     const scenarios = useSelector(getScenarios);
-
+    const [ scenarioSize, setScenarioSize] = useState("ALL" );
+    function sizeFilter(scenario) { return scenarioSize === "ALL" || scenario.size === scenarioSize}; 
+    
+    
     return (
         <>
-        <FilterScenarioSize />
+        <div className="horizontalBlocks">
+            { buttons.map((button, i) =><button key={i} onClick={() =>setScenarioSize(button)} >{button}</button>)}
+        </div>
+
         <div>Scenario List:</div>
         <ul>
-            {scenarios?.map((scenario, i) => <li key={i}><button  onClick={() => dispatch(scenarioSettings(scenario))}>{scenario.name}</button></li>)}
+            {scenarios?.filter(sizeFilter).map((scenario, i) => <li key={i}><button  onClick={() => dispatch(scenarioSettings(scenario))}>{scenario.name} {scenario.size}</button></li>)}
         </ul>
         <Button text="OKAY" action={closeAction} />
         </>
